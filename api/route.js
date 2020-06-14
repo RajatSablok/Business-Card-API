1d1e7adc2623b75a3ad0cbea660f7d27e7951967
RajatSablok <rajat.main06@gmail.com>
2020-06-14 23:40:01 +0530

    terminal output ez

---
+++ api/route.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const chalk = require("chalk");
+const boxen = require("boxen");
+const path = require("path");
+const fs = require("fs");
+const router = express.Router();
+
+// set up Boxen options
+const boxenOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+};
+
+router.get("/app", (req, res) => {
+  //   console.log(req.body);
+  const linkedin = req.body.linkedin;
+  const github = req.body.github;
+  const name = req.body.name;
+  const newLine = "\n";
+  //   console.log("gege");
+
+  const result =
+    chalk.blue(`                Hello, I'm name ${name}`) +
+    newLine +
+    newLine +
+    chalk.red(`   LinkedIn: `) +
+    chalk.cyan(`${linkedin}`) +
+    newLine +
+    chalk.red(`   GitHub: `) +
+    chalk.cyan(`${github}`) +
+    newLine +
+    newLine +
+    chalk.yellow(
+      "Written with love by e33or_assasin, modified by Rajat Sablok"
+    ) +
+    newLine;
+
+  console.log(boxen(result));
+  res.status(200).send(result);
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,47 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const chalk = require("chalk");
+const boxen = require("boxen");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
+
+const routes = require("./api/route");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use("/api", routes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found 123");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
